7b7a89ff5146197fcaedce72eaeaca308b5d4b1b
MAYANK-M31 <mayank31m@gmail.com>
2024-07-19 13:12:26 +0530

    init

---
+++ server.js
@@ -0,0 +1,24 @@
+// Import the framework and instantiate it
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler(request, reply) {
+  return { success: true };
+});
+
+// Declare a route
+fastify.post("/reservation", async function handler(request, reply) {
+  console.log("PAYLOAD", request.body, request.query, request.params);
+  return { success: true };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 7001 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
